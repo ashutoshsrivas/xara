6ed29a4b777a4c534cb0d8a5a33edd67e16ff38d
ashutoshsrivastava9897 <root@ashutoshs-MacBook-Air.local>
2023-09-12 15:12:32 +0530

    sept-12

--- frontend/xara/pages/profile/index.tsx
+++ frontend/xara/pages/profile/index.tsx
@@ -2,28 +2,116 @@
 import { Router, useRouter } from "next/router";
 import { useAuthContext } from "../../lib/context/AuthContext";
 import { useEffect, useState } from "react";
-import  Mydrawer from "../components/Mydrawer";
+import Mydrawer from "../components/Mydrawer";
 const index = () => {
   const {
-    user
+    user,
   }: {
     user?: object | any;
   } = useAuthContext();
 
-  const [email, setEmail] = user!=null?useState(user.email):useState("");
+  const [email, setEmail] = user != null ? useState(user.email) : useState("");
   const router = useRouter();
-  
+
   useEffect(() => {
-    if (!user) router.push("../");
+    if (!user) router.push("../signin?callback=profile");
   }, [user]);
 
-  
   return (
     <div>
       <Mydrawer activetab="profile"></Mydrawer>
       <div className="container m-0-auto">
-        <h1>My Profile</h1>  
-        <p>Your current Primary email address is <span className="color-red">{email}</span></p>
+        <div className="px-5">
+          <h1>My Profile</h1>
+          <p>
+            Your current Primary email address is{" "}
+            <span className="color-red">{email}</span>
+          </p>
+        </div>
+
+        <div className="px-5">
+          <p className="profile-subheading">Basic information</p>
+          <p>
+            Update some personal information. Your address will never be
+            publicly available.
+          </p>
+        </div>
+        <div className="px-5 py-2">
+          <div className="container row">
+            <div className="col-sm-4">
+              <label className="profile-label ">First Name</label>
+            </div>
+            <div className="col-sm-4">
+              <input
+                className="profile-input form-control my-2"
+                type="text"
+                placeholder="First Name"
+              ></input>
+            </div>
+            <div className="col-sm-4">
+              <input
+                className="profile-input form-control my-2"
+                type="text"
+                placeholder="Last Name"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div className="px-5 py-2">
+          <div className="container row">
+            <div className="col-sm-4 my-2">
+              <label>Email</label>
+            </div>
+            <div className="col-sm-8">
+              <input
+                className="profile-input form-control "
+                type="email"
+                placeholder="Email"
+              ></input>
+            </div>
+          </div>
+        </div>
+        
+        <div className="px-5 py-2">
+          <div className="container row">
+            <div className="col-sm-4 my-2">
+              <label>Phone</label>
+            </div>
+            <div className="col-sm-8">
+              <input
+                className="profile-input form-control "
+                type="text"
+                placeholder="Phone"
+              ></input>
+            </div>
+          </div>
+        </div>
+        
+        <div className="px-5 py-2">
+          <div className="container row">
+            <div className="col-sm-4">
+              <label>Address</label>
+            </div>
+            <div className="col-sm-8">
+              <textarea className="form-control my-2">
+              </textarea>
+            </div>
+          </div>
+        </div>
+        
+        <div className="px-5 py-2">
+          <div className="container row">
+            <div className="col-sm-4">
+              
+            </div>
+            <div className="col-sm-8">
+            <button className="btn-save p-2"> Save Changes </button>
+            </div>
+          </div>
+        </div>
+        
+        
+
       </div>
     </div>
   );
---
+++ frontend/xara/pages/signin/index.tsx
@@ -0,0 +1,180 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+// import Link from '@mui/material/Link';
+import Link from "next/link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useAuthContext } from "../../lib/context/AuthContext";
+import { useRouter } from "next/router";
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://windikate.com/">
+        Windikate Technologies Private Limited
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignInSide() {
+  const {
+    user,
+    googleSignIn,
+    logOut,
+    createuser,
+  }: {
+    user?: object | any;
+    googleSignIn?: () => void;
+    logOut?: () => void;
+    createuser?: (id: string, pass: string) => void;
+  } = useAuthContext();
+  const router = useRouter();
+  
+
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn?.();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut?.();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  React.useEffect(() => {
+    const { callback } = router.query;
+    const callbackurl = callback !== undefined?`/${callback}`:"/explore";
+    console.log(router.query);
+    if (user) router.push(callbackurl);
+  }, [user]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={6}
+          md={6}
+          sx={{
+            backgroundImage: "url(bg.svg)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={6} md={6} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+            }}
+            className="padding-login-box py-3"
+          >
+            <Typography component="h1" variant="h4">
+              Welcome Back
+            </Typography>
+            <p>
+              Don't have an account? <Link href="/register">Sign up</Link>
+            </p>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <small className="text-sm">
+                Use 8 or more characters with a mix of letters, numbers &
+                symbols
+              </small>
+              <br></br>
+              <br></br>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 1, mb: 1 }}
+                className="btn-login p-2"
+              >
+                Log In
+              </Button>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 1, mb: 1 }}
+                className="btn-login-google p-2"
+                onClick={handleSignIn}
+              >
+                <img src="googleLogo.svg" className="mx-2" />
+                <span>Continue with google</span>
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
